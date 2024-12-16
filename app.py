ed451a642df278c430f6512e02132e7c89e78827
miqueasmorosin1 <morosinmiqueasg@gmail.com>
2024-12-16 19:13:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -26,73 +26,24 @@ st.set_page_config(
 )
 def add_particles():
     particles_html = """
-    <!-- particles.js container -->
     <div id="particles-js"></div>
 
-    <!-- stats - count particles -->
-    <div class="count-particles">
-        <span class="js-count-particles">--</span> particles
-    </div>
-
-    <!-- styles -->
     <style>
-        /* ---- reset ---- */
-        body {
-            margin: 0;
-            font: normal 75% Arial, Helvetica, sans-serif;
-        }
-        canvas {
-            display: block;
-            vertical-align: bottom;
-        }
-        /* ---- particles.js container ---- */
+        /* Configuración del contenedor */
         #particles-js {
-            position: absolute;
+            position: fixed; /* Se asegura de que el contenedor esté fijo */
+            top: 0;
+            left: 0;
             width: 100%;
             height: 100%;
+            z-index: -1; /* Para que esté detrás del contenido principal */
             background-color: #ffffff; /* Fondo blanco */
-            background-image: url("");
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: 50% 50%;
-        }
-        /* ---- stats.js ---- */
-        .count-particles {
-            background: #000022;
-            position: absolute;
-            top: 48px;
-            left: 0;
-            width: 80px;
-            color: #13E8E9;
-            font-size: .8em;
-            text-align: left;
-            text-indent: 4px;
-            line-height: 14px;
-            padding-bottom: 2px;
-            font-family: Helvetica, Arial, sans-serif;
-            font-weight: bold;
-        }
-        .js-count-particles {
-            font-size: 1.1em;
-        }
-        #stats, .count-particles {
-            -webkit-user-select: none;
-            margin-top: 5px;
-            margin-left: 5px;
-        }
-        #stats {
-            border-radius: 3px 3px 0 0;
-            overflow: hidden;
-        }
-        .count-particles {
-            border-radius: 0 0 3px 3px;
         }
     </style>
 
-    <!-- scripts -->
-    <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-    <script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
     <script>
+        /* Inicialización de las partículas */
         particlesJS("particles-js", {
             "particles": {
                 "number": {
@@ -110,35 +61,15 @@ def add_particles():
                     "stroke": {
                         "width": 0,
                         "color": "#000000"
-                    },
-                    "polygon": {
-                        "nb_sides": 5
-                    },
-                    "image": {
-                        "src": "img/github.svg",
-                        "width": 100,
-                        "height": 100
                     }
                 },
                 "opacity": {
                     "value": 0.5,
-                    "random": false,
-                    "anim": {
-                        "enable": false,
-                        "speed": 1,
-                        "opacity_min": 0.1,
-                        "sync": false
-                    }
+                    "random": false
                 },
                 "size": {
                     "value": 3,
-                    "random": true,
-                    "anim": {
-                        "enable": false,
-                        "speed": 40,
-                        "size_min": 0.1,
-                        "sync": false
-                    }
+                    "random": true
                 },
                 "line_linked": {
                     "enable": true,
@@ -154,12 +85,7 @@ def add_particles():
                     "random": false,
                     "straight": false,
                     "out_mode": "out",
-                    "bounce": false,
-                    "attract": {
-                        "enable": false,
-                        "rotateX": 600,
-                        "rotateY": 1200
-                    }
+                    "bounce": false
                 }
             },
             "interactivity": {
@@ -174,56 +100,14 @@ def add_particles():
                         "mode": "push"
                     },
                     "resize": true
-                },
-                "modes": {
-                    "grab": {
-                        "distance": 400,
-                        "line_linked": {
-                            "opacity": 1
-                        }
-                    },
-                    "bubble": {
-                        "distance": 400,
-                        "size": 40,
-                        "duration": 2,
-                        "opacity": 8,
-                        "speed": 3
-                    },
-                    "repulse": {
-                        "distance": 200,
-                        "duration": 0.4
-                    },
-                    "push": {
-                        "particles_nb": 4
-                    },
-                    "remove": {
-                        "particles_nb": 2
-                    }
                 }
             },
             "retina_detect": true
         });
-        var count_particles, stats, update;
-        stats = new Stats;
-        stats.setMode(0);
-        stats.domElement.style.position = 'absolute';
-        stats.domElement.style.left = '0px';
-        stats.domElement.style.top = '0px';
-        document.body.appendChild(stats.domElement);
-        count_particles = document.querySelector('.js-count-particles');
-        update = function () {
-            stats.begin();
-            stats.end();
-            if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-                count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-            }
-            requestAnimationFrame(update);
-        };
-        requestAnimationFrame(update);
     </script>
     """
     # Renderizar el HTML en Streamlit
-    st.components.v1.html(particles_html, height=600)
+    st.components.v1.html(particles_html, height=0, width=0)
 st.title("Embryo Transfer Prioritization")
 add_particles()
 threshold = 0.8
